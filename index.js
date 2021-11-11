a4242dc5e8ebb5a560988e4b8809fab0f17f802d
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-11-11 09:07:54 +0530

    Expres Server and routes

--- index.js
+++ index.js
@@ -1,56 +1,27 @@
-const http = require("http");
-const PORT = 3000;
+// create a new express server
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-function handler(req, res) {
-  switch (req.url) {
-    case "/":
-      res.write("Hello World!!");
-      break;
-    case "/Users":
-      const users = [
-        {
-          name: "Ivan Jensen",
-          phone: "(189) 387-6798",
-          email: "donec.vitae@uterat.ca",
-          country: "Italy",
-        },
-        {
-          name: "Aaron Osborne",
-          phone: "(743) 789-7413",
-          email: "convallis.dolor@egetmetuseu.org",
-          country: "Italy",
-        },
-        {
-          name: "Lysandra Alvarez",
-          phone: "(692) 862-1877",
-          email: "donec.fringilla@quisquefringilla.edu",
-          country: "Canada",
-        },
-        {
-          name: "Chaim Gonzales",
-          phone: "1-484-273-4261",
-          email: "quis@euismod.co.uk",
-          country: "Germany",
-        },
-        {
-          name: "Inez Levy",
-          phone: "(660) 162-6958",
-          email: "donec.felis.orci@nullamfeugiat.org",
-          country: "Nigeria",
-        },
-      ];
-      res.write(JSON.stringify(users));
-      break;
-    case "/names":
-      res.write(JSON.stringify("Names"));
-      break;
-    default:
-      res.write("Not Found ");
-      break;
-  }
-  res.end();
-}
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-const server = http.createServer(handler);
+// route or endpoint
+app.get("/", (req, res) => {
+  res.send("hello express");
+});
 
-server.listen(PORT, () => console.log("server is running at port", PORT));
+app.get("/books", (req, res) => {
+  const books = [
+    { id: 1, name: "Clean Code", price: 100 },
+    { id: 2, name: "DS & Alg", price: 200 },
+    { id: 3, name: "Eloquent JavaScript", price: 120 },
+  ];
+  res.status(200);
+  res.json(books);
+});
+
+app.get("/authors", (req, res) => {
+  res.status(200).send("Authors");
+});

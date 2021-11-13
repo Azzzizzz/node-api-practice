c699b908f1f06f0fb8e5e6a962b15ca68fb9caf4
Syed Abdul Aziz <syedaziz9999@gmail.com>
2021-11-13 12:02:52 +0530

    Add Repositories Layer

--- controllers/bookCtrl.js
+++ controllers/bookCtrl.js
@@ -1,22 +1,22 @@
 const Book = require("../models/bookModel");
+const bookRepository = require("../repositories/bookRepository");
 
 const books = [
   { id: 1, name: "Clean Code", price: 100 },
   { id: 2, name: "DS & Alg", price: 200 },
   { id: 3, name: "Eloquent JavaScript", price: 120 },
 ];
-const get = (req, res) => {
-  Book.find(),
-    pretty()
-      .then(function (books) {
-        res.status(200);
-        res.json(books);
-      })
-      .catch((err) => {
-        // TODO: log err
-        res.status(500);
-        res.send("Internal server Error");
-      });
+
+const get = async (req, res) => {
+  try {
+    const books = await bookRepository.getAll();
+    res.status(200);
+    res.json(books);
+  } catch (e) {
+    // TODO: Logging
+    res.status(500);
+    res.send("Internal server error");
+  }
 };
 
 const post = (req, res) => {
